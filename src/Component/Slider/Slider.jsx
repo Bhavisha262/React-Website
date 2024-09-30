223e3bdf64dd2123580a6f8338cb257a6cfb4182
Bhavisha262 <138797566+Bhavisha262@users.noreply.github.com>
2024-09-30 23:08:48 +0530

    bhavisha

--- src/Component/Slider/Slider.jsx
+++ src/Component/Slider/Slider.jsx
@@ -4,14 +4,13 @@ import CoreStyles from 'react-awesome-slider/src/core/styles.scss';
 import AnimationStyles from 'react-awesome-slider/src/styled/cube-animation/cube-animation.scss';
 import "./Slider.scss";
 
-import img1 from "../../Assets/Slider/Sinful-Lipstick-Banner-Desktop1.jpg";
-import img2 from "../../Assets/Slider/Nudes_HP-Banner_Med_Desktop_1.jpg";
-import img3 from "../../Assets/Slider/05_2024_TeaParty_Launch_Banner_2000x.progressive.jpg";
-import img4 from "../../Assets/Slider/Glycolic-desktop-homepage_1_1400x.jpg";
-import img5 from "../../Assets/Slider/SINFUL-PLUMP-LIP-GLOSS-OP-1_web.jpg";
-import img6 from "../../Assets/Slider/F14_Desk_top_size.jpg";
-import img7 from "../../Assets/Slider/Homepage_banner-02-02_1400x.jpg";
-
+import img1 from "../../Assets/Slider/img1.png";
+import img2 from "../../Assets/Slider/img2.jpg";
+import img3 from "../../Assets/Slider/img3.png";
+import img4 from "../../Assets/Slider/img4.png";
+import img5 from "../../Assets/Slider/img5.jpg";
+import img6 from "../../Assets/Slider/img6.jpg";
+import img7 from "../../Assets/Slider/img7.jpg";
 
 
 const Slider = () => {
